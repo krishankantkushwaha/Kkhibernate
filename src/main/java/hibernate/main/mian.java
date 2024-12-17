19f823d09769a23d2978aafc11636d408ba16918
krishankantkushwaha <krishankantkushwaha719@gmail.com>
2024-12-17 18:51:35 +0530

    update

--- src/main/java/hibernate/main/mian.java
+++ src/main/java/hibernate/main/mian.java
@@ -16,25 +16,10 @@ public class mian {
 	public static void main(String[] args) {
 //		HEmployee emp = new HEmployee(1,"krishna","male",65533);
 		
-		                     // auto generate id 
-		HEmployee emp = new HEmployee("love","male",55262);
-		
-		
-//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-//		SessionFactory sf = cfg.buildSessionFactory();
-//		Session session = sf.openSession();
-//		Transaction tx = session.beginTransaction();
-//		session.persist(emp);
-//		tx.commit();
-//		System.out.println("mian.main()");
-		
-		
-		
 		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
 //		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
 //		SessionFactory sf = meta.buildSessionFactory();
 		SessionFactory sf = new MetadataSources( new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build()).getMetadataBuilder().build().buildSessionFactory();
-		
 		Session session = sf.openSession();
 		session.persist(emp);
 		session.beginTransaction().commit();
