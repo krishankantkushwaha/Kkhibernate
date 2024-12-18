f15b2f3827e14b822b6f430036e7fc7bb0fdc33b
krishankantkushwaha <krishankantkushwaha719@gmail.com>
2024-12-18 19:15:03 +0530

    update

--- src/main/java/hibernate/main/mian.java
+++ src/main/java/hibernate/main/mian.java
@@ -17,52 +17,48 @@ import hibernate.entity.HEmployee;
 public class mian {
 
 	public static void main(String[] args) {
-//		HEmployee emp = new HEmployee(7, "raj", "male", 36538, "TCS");
+		HEmployee emp = new HEmployee(7, "raj", "male", 36538, "TCS");
 		SessionFactory sf = HibernateConfiguration.getSaessionFactory();
 		Session session = sf.openSession();
 
 // 		session.save(emp);
 //		session.beginTransaction().commit();
 
-		// Defference load and get yadi object nhi milega (mtlb glt dalne pr) to load method exception dega or get method null return karega
-//		HEmployee em = session.get(HEmployee.class,3);
 
-//		HEmployee em = session.load(HEmployee.class, 38);
-//        System.out.println(em);
+//		HQL concept //
+			// Example of hql to get all records
+			Query query = session.createQuery("from emp2");
+			List<HEmployee> list = query.list();
+			System.out.println(list);
 
-//	HQL concept //
-		// Example of hql to get all records
-//		Query query = session.createQuery("from emp2");
-//		List<HEmployee> list = query.list();
-//		System.out.println(list);
+			// for each use kiya hai line by line traversing ke liye/
+//			for(HEmployee e:list) {
+//				System.out.println(e);
+//			}
 
-		// for each use kiya hai line by line traversing ke liye/
-//		for(HEmployee e:list) {
-//			System.out.println(e);
-//		}
+			// Example of hql to get records with Pagination
+//			Query query = session.createQuery("from emp2");
+//			query.setFirstResult(4);
+//		query.setMaxResults(3);
+//			List list = query.list();
+//			System.out.println(list);
 
-		// Example of hql to get records with Pagination
-//		Query query = session.createQuery("from emp2");
-//		query.setFirstResult(4);
-//	query.setMaxResults(3);
-//		List list = query.list();
-//		System.out.println(list);
+			// Example of hql update Query using named parameter
 
-		// Example of hql update Query using named parameter
+//			Transaction tx = session.beginTransaction();
+//			Query query = session.createQuery("update emp2 set name=:n where id=:i");
+//			query.setParameter("n","Ramprakash");
+//			query.setParameter("i",1);
+//			int status = query.executeUpdate();
+//			System.out.println(status);
+//			tx.commit();
 
-//		Transaction tx = session.beginTransaction();
-//		Query query = session.createQuery("update emp2 set name=:n where id=:i");
-//		query.setParameter("n","Ramprakash");
-//		query.setParameter("i",1);
-//		int status = query.executeUpdate();
-//		System.out.println(status);
-//		tx.commit();
-
-		// Example of hql delet Query
-		Query query = session.createQuery("delete from emp2  where id=1");
-		query.executeUpdate();
-		System.out.println("mian.main()");
+			// Example of hql delet Query
+//			Query query = session.createQuery("delete from emp2  where id=1");
+//			query.executeUpdate();
 		sf.close();
+		System.out.println("mian.main()");
+	
 	}
 
 }
